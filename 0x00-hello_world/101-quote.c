b8319b6b8a31e80a9c3fcb4693aa2d56ac95e058
kennan <kenanusheta24@gmail.com>
2023-02-16 15:11:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write("and that price of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	fput("and that price of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
