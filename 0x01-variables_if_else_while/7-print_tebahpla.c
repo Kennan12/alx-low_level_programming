be05a777211ac059bd9d583d975796d384a7d706
kennan <kenanusheta24@gmail.com>
2023-02-18 08:48:47 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char k;
+
+	k = 'z';
+	while
+		(k >= 'a') {
+			putchar(k);
+			k--;
+		}
+	putchar('\n');
+	return (0);
+}
