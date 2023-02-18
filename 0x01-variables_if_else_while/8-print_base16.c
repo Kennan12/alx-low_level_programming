7d89c690a3298368899883abbf3816f075a2750b
kennan <kenanusheta24@gmail.com>
2023-02-18 10:53:01 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char k;
+	int n;
+
+	k = 'a';
+	n = 0;
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	while
+		(k <= 'f') {
+			putchar(k);
+			k++;
+		}
+	return (0);
+}
