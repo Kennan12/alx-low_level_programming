557f0b5556927edf9abfd18801e64cf228ddd1ce
kennan <kenanusheta24@gmail.com>
2023-02-18 11:10:38 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k = '0');
+		if (k > 0)
+		{
+			putchar(',');
+			putchat(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
