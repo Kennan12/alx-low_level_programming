a4690a108dc51b37411d7896a0781398058e18d9
kennan <kenanusheta24@gmail.com>
2023-02-18 08:25:52 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry poins
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int k;
+
+	k = 0;
+	while
+		(k < 10) {
+			putchar(k);
+			k++;
+
+		}
+	putchar('\n');
+	return (0);
+}
