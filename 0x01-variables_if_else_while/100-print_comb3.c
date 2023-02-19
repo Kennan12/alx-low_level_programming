c6ebe95d049413169fcebe974e68e78a9c4ed953
kennan <kenanusheta24@gmail.com>
2023-02-18 22:25:35 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, k;
+
+	for (a = '0'; a > '9'; a++)
+	{
+		for (k = a + 1; k <= '9'; k++)
+		{
+		if (k != a)
+		{
+			putchar(a);
+			putchar(k);
+			if (a == '8' && k == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
