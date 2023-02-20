47961705cf4dcc592e24257c891dbc420d1e885d
kennan <kenanusheta24@gmail.com>
2023-02-20 07:28:35 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,22 +12,22 @@ int main(void)
 
 	for (a = '0'; a < '9'; a++)
 	{
-		for (b = 'a + 1'; b < '9'; b++)
-		{
-			for (c = 'b + 1'; c < '9'; c++)
-			{
-				if ((b != a) != c)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(c);
-					if (a == '7' && b == '8')
-						continue;
-						putchar(',');
-						putchar(' ');
-				}
-			}
-		}
+	for (b = a + 1; b < '9'; b++)
+	{
+	for (c = b + 1; c < '9'; c++)
+	{
+	if ((b != a) != c)
+	{
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == '7' && b == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
