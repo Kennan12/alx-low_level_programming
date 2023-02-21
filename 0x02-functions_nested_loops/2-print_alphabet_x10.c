266ee6faaf5f4ea4547f9ff37d41d3651a249a08
kennan <kenanusheta24@gmail.com>
2023-02-21 08:04:02 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_10x - My alphabet x10 times
+ * print_alphabet_x10 - My alphabet x10 times
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	char n;
