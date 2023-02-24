c4573d78c7ef0f0a40b48bed9a1643172e770e5d
kennan <kenanusheta24@gmail.com>
2023-02-24 07:04:19 -0800

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check a characters
+ * @c: char to be checked
+ *
+ * Return: 1 for a character that or 0 any else
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
